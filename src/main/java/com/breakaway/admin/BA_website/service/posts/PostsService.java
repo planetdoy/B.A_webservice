06d39e6939f37d1e0306ec65127cedabb9ce0e57
planetdoy <doydoit@gmail.com>
2020-09-09 17:59:15 +0900

    등록/수정/조회 API 만들기 - Request 데이터를 받을 Dto (PostsSaveRequestDto 생성) - API 요청을 받을 Controller (PostsApiController 생성) - 트렌잭션, 도메인 기능 간의 순서를 보장하는 Service (PostsService 생성)

---
+++ src/main/java/com/breakaway/admin/BA_website/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.breakaway.admin.BA_website.service.posts;
+
+import com.breakaway.admin.BA_website.domain.posts.PostsRepository;
+import com.breakaway.admin.BA_website.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/breakaway/admin/BA_website/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,28 @@
+package com.breakaway.admin.BA_website.web.dto;
+
+import com.breakaway.admin.BA_website.domain.posts.Posts;
+import lombok.Builder;
+
+public class PostsSaveRequestDto {
+
+     private String title;
+     private String content;
+     private String author;
+
+     @Builder
+     public PostsSaveRequestDto(String title, String content, String author){
+
+         this.title = title;
+         this.content = content;
+         this.author = author;
+
+     }
+
+     public Posts toEntity(){
+         return Posts.builder()
+                 .title(title)
+                 .content(content)
+                 .author(author)
+                 .build();
+     }
+}
