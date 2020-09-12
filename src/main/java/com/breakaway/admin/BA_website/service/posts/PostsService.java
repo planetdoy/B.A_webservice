92f634d925d73f2ef16e5b00c23ad5bf91d3a5f4
planetdoy <doydoit@gmail.com>
2020-09-13 00:13:29 +0900

    등록/수정/조회 API 만들기 - PostsApiController에 update 기능 추가
    - PostsApiControllerTest에 테스트 코드 작성

--- src/main/java/com/breakaway/admin/BA_website/service/posts/PostsService.java
+++ src/main/java/com/breakaway/admin/BA_website/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.breakaway.admin.BA_website.service.posts;
 
+import com.breakaway.admin.BA_website.domain.posts.Posts;
 import com.breakaway.admin.BA_website.domain.posts.PostsRepository;
+import com.breakaway.admin.BA_website.web.dto.PostsResponseDto;
 import com.breakaway.admin.BA_website.web.dto.PostsSaveRequestDto;
+import com.breakaway.admin.BA_website.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id ));
+
+        posts.update(requestDto.getTitle() ,requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+
     }
 }
--- src/main/java/com/breakaway/admin/BA_website/web/PostsApiController.java
+++ src/main/java/com/breakaway/admin/BA_website/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.breakaway.admin.BA_website.web;
 
 import com.breakaway.admin.BA_website.service.posts.PostsService;
+import com.breakaway.admin.BA_website.web.dto.PostsResponseDto;
 import com.breakaway.admin.BA_website.web.dto.PostsSaveRequestDto;
+import com.breakaway.admin.BA_website.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,4 +18,15 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+
+            return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+       return postsService.findById(id);
+    }
 }
+
