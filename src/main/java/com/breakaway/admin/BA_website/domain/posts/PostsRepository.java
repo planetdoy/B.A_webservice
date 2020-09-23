68f337b8180a57a98892dec9865337f63fa17e9f
planetdoy <doydoit@gmail.com>
2020-09-23 20:27:53 +0900

    PostsRepository에 @Query 추가

--- src/main/java/com/breakaway/admin/BA_website/domain/posts/PostsRepository.java
+++ src/main/java/com/breakaway/admin/BA_website/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.breakaway.admin.BA_website.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
     JpaRepository<Entity클래스 ,PK 타입>를 상속하면
@@ -8,4 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 * */
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
