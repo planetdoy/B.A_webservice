2c73e4cfaba4d4bea6167ad12ce2af236ea23f2b
planetdoy <doydoit@gmail.com>
2020-09-24 15:22:52 +0900

    게시물 조회를 위한 IndexController 수정  - Model을 이용한 객체 전달
    PostsService findAllDesc() 메소드 추가
     - 게시물 리스트를 받아옵니다.
    
    PostsListResponseDto 생성
     - 게시물 객체를 담을 dto

--- src/main/java/com/breakaway/admin/BA_website/service/posts/PostsService.java
+++ src/main/java/com/breakaway/admin/BA_website/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.breakaway.admin.BA_website.service.posts;
 
 import com.breakaway.admin.BA_website.domain.posts.Posts;
 import com.breakaway.admin.BA_website.domain.posts.PostsRepository;
+import com.breakaway.admin.BA_website.web.dto.PostsListResponseDto;
 import com.breakaway.admin.BA_website.web.dto.PostsResponseDto;
 import com.breakaway.admin.BA_website.web.dto.PostsSaveRequestDto;
 import com.breakaway.admin.BA_website.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +39,21 @@ public class PostsService {
         return new PostsResponseDto(entity);
 
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        //.map(posts -> new PostsListResponseDto(posts))
+        /*
+            postsRespository 결과로 넘어온 Posts의 Stream을 map을 통해
+            PostsListResponseDto 변환 -> List로 반환하는 메소드입니다.
+
+            @Transactional(readOnly = true)
+                - 조회 기능만 남겨두어 조회 속도가 개선됩니다.
+                - 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것을 추천합니다.
+        * */
+    }
+
 }
--- src/main/java/com/breakaway/admin/BA_website/web/IndexController.java
+++ src/main/java/com/breakaway/admin/BA_website/web/IndexController.java
@@ -1,16 +1,26 @@
 package com.breakaway.admin.BA_website.web;
 
+import com.breakaway.admin.BA_website.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+        /*
+            Model
+                서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
+                여기서는 posts.Service.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달합니다.
+        * */
     }
 
     @GetMapping("/posts/save")
