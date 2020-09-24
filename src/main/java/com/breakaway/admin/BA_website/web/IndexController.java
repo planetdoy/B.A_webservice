dc4a0bbacd203e818a039b60b6eb1b86f0f07dff
planetdoy <doydoit@gmail.com>
2020-09-24 17:05:43 +0900

    수정화면으로 연결하기 위해 IndexController 코드 작업  - postsUpdate 메소드 생성

--- src/main/java/com/breakaway/admin/BA_website/web/IndexController.java
+++ src/main/java/com/breakaway/admin/BA_website/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.breakaway.admin.BA_website.web;
 
 import com.breakaway.admin.BA_website.service.posts.PostsService;
+import com.breakaway.admin.BA_website.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,6 +28,15 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
     }
 
 
