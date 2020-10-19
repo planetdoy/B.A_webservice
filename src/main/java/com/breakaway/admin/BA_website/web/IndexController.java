ffb27054fb2fd0b558bbecf46660953fca1fcaa0
planetdoy <doydoit@gmail.com>
2020-10-20 00:14:14 +0900

    어노테이션 기반으로 개선하기
    SessionUser user = (SessionUser) httpsSession.getAttribute("user");
    -> @LoginUser Session user
    
     - 어노테이션 @LoginUser 생성
     - 조건이 맞는 메소드가 있다면 구현체가 지정한 값을 해당 메소드의 파라미터로 넘기는
     HandlerMethodArgumentResolver 인터페이스를 구현한 LoginUserArgumentResolver 클래스 생성
    
     - LoginUserArgumentResolver가 스프링에서 인식될 수 있도록 WebMvcConfigurer에 추가
     config 패키지에 WebConfig 클래스 생성

--- src/main/java/com/breakaway/admin/BA_website/web/IndexController.java
+++ src/main/java/com/breakaway/admin/BA_website/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.breakaway.admin.BA_website.web;
 
+import com.breakaway.admin.BA_website.config.auth.LoginUser;
 import com.breakaway.admin.BA_website.config.auth.dto.SessionUser;
 import com.breakaway.admin.BA_website.service.posts.PostsService;
 import com.breakaway.admin.BA_website.web.dto.PostsResponseDto;
@@ -9,20 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){              // (1-2)
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // (1)
+        /*
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");      // (1-1)
+        */
+
         if(user != null){                                                       // (2)
             model.addAttribute("userName", user.getName());
         }
@@ -32,12 +33,17 @@ public class IndexController {
                 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
                 여기서는 posts.Service.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달합니다.
 
-             1. (SessionUser) httpSession.getAttribute("user")
+             1-1. (SessionUser) httpSession.getAttribute("user")
                 - 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성했습니다.
                 - 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있습니다.
+
              2. if(user != null)
                 - 세션에 저장된 값이 있을 때만 model에 userName으로 등록합니다.
                 - 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됩니다.
+
+             1-2. @LoginUser SessionUser user
+                - 기존에 (SessionUser) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선 되었습니다.
+                - 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었습니다.
         * */
     }
 
